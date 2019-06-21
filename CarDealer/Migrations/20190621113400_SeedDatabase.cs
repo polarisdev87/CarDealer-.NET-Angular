8c575f7d4205e7c352c95606df28fdf6ada8f829
polaris87 <polarisdev87@outlook.com>
2019-06-21 13:47:12 +0200

    Add Seed-Data

--- CarDealer/Migrations/20190621113400_SeedDatabase.cs
+++ CarDealer/Migrations/20190621113400_SeedDatabase.cs
@@ -6,12 +6,22 @@ namespace CarDealer.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Mercedes-Benz')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Porsche')");
 
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('S-Klasse Limousine', (SELECT Id FROM Makes WHERE Name = 'Mercedes-Benz'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('G-Klasse', (SELECT Id FROM Makes WHERE Name = 'Mercedes-Benz'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('AMG GT 63 S 4MATIC+', (SELECT Id FROM Makes WHERE Name = 'Mercedes-Benz'))");
+
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('911 Speedster', (SELECT Id FROM Makes WHERE Name = 'Porsche'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) VALUES ('Panamera GTS Sport Turismo',( SELECT Id FROM Makes WHERE Name = 'Porsche'))");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql("DELETE FROM Makes");
         }
     }
 }
