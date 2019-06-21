0c8d7556d6cc9a34a15d90f38fe8d8c9d001f94e
polaris87 <polarisdev87@outlook.com>
2019-06-21 14:08:43 +0200

    Add Mapper for MakesController

--- CarDealer/Controllers/MakesController.cs
+++ CarDealer/Controllers/MakesController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using CarDealer.Core.Domain;
 using CarDealer.Infrastructure;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarDealer.Controllers
 {
@@ -13,17 +15,21 @@ namespace CarDealer.Controllers
     public class MakesController : ControllerBase
     {
         private readonly CarDealerDbContext context;
+        private readonly IMapper mapper;
 
-        public MakesController(CarDealerDbContext context)
+        public MakesController(CarDealerDbContext context, IMapper mapper)
         {
+
+            this.mapper = mapper;
             this.context = context;
+
         }
 
 
         [HttpGet("/api/makes")]
-        public IEnumerable<Make> GetMakes()
+        public async Task<IEnumerable<Make>> GetMakes()
         {
-
+            return await context.Makes.Include(m => m.Models).ToListAsync();
         }
 
     }
