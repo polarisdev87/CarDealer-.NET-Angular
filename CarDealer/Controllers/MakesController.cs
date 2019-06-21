e6298ead6795a5c215414b2e91e1401453b86777
polaris87 <polarisdev87@outlook.com>
2019-06-21 13:54:49 +0200

    Add Constructor to MakesController to get DbContext

--- CarDealer/Controllers/MakesController.cs
+++ CarDealer/Controllers/MakesController.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CarDealer.Core.Domain;
+using CarDealer.Infrastructure;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarDealer.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
     public class MakesController : ControllerBase
     {
+        private readonly CarDealerDbContext context;
 
+        public MakesController(CarDealerDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        [HttpGet("/api/makes")]
         public IEnumerable<Make> GetMakes()
         {
 
