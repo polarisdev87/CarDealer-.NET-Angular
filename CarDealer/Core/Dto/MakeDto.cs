b7f595e47939fb57b87361fc662aebb78899a559
polaris87 <polarisdev87@outlook.com>
2019-06-21 14:02:34 +0200

    Add MakeDto

---
+++ CarDealer/Core/Dto/MakeDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using CarDealer.Core.Domain;
+
+namespace CarDealer.Core.Dto
+{
+    public class MakeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Model> Models { get; set; }
+
+
+        public MakeDto()
+        {
+            Models = new Collection<Model>();
+        }
+
+    }
+}
