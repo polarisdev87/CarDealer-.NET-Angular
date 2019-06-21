21e583e8269e497b61ef0fc3dc7e519af6ac6308
polaris87 <polarisdev87@outlook.com>
2019-06-21 13:52:21 +0200

    Create first Api Route in MakesController

--- CarDealer/Controllers/MakesController.cs
+++ CarDealer/Controllers/MakesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarDealer.Core.Domain;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,11 @@ namespace CarDealer.Controllers
     [ApiController]
     public class MakesController : ControllerBase
     {
+
+        public IEnumerable<Make> GetMakes()
+        {
+
+        }
+
     }
 }
