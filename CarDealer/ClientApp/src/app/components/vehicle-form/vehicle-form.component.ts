54a6ec35eee55b8c00ec94802d173970ec436376
polaris87 <polarisdev87@outlook.com>
2019-06-22 14:06:00 +0200

    Declare field "makes" in VehicleFormComponent

--- CarDealer/ClientApp/src/app/components/vehicle-form/vehicle-form.component.ts
+++ CarDealer/ClientApp/src/app/components/vehicle-form/vehicle-form.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehicleFormComponent implements OnInit {
 
+  makes;
+
   constructor(private makeService: MakeService) { }
 
   ngOnInit() {
