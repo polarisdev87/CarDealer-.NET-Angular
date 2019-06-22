f3802ad01be91c50fe3b3099c8b2ed07e6c10794
polaris87 <polarisdev87@outlook.com>
2019-06-22 18:01:36 +0200

    Create FeaturesController.cs

---
+++ CarDealer/Controllers/FeaturesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CarDealer.Core.Domain;
+using CarDealer.Core.Dto;
+using CarDealer.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarDealer.Controllers
+{
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        private readonly CarDealerDbContext context;
+        private readonly IMapper mapper;
+
+        public FeaturesController(CarDealerDbContext context, IMapper mapper)
+        {
+
+            this.mapper = mapper;
+            this.context = context;
+
+        }
+
+
+        [HttpGet("/api/features")]
+        public async Task<IEnumerable<FeatureDto>> GetFeatures()
+        {
+            var features = await context.Features.ToListAsync();
+
+            return mapper.Map<List<Feature>, List<FeatureDto>>(features);
+        }
+
+    }
+}
