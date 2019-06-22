82a3e8e7e3fb64badc05f63523a46b36bc3d8fc5
polaris87 <polarisdev87@outlook.com>
2019-06-22 18:23:56 +0200

    Add some features to seed the featurestable in database

---
+++ CarDealer/Migrations/20190622161914_SeedFeaturesTable.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarDealer.Migrations
+{
+    public partial class SeedFeaturesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('360 Degree Camera')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Built-in 4G LTE Wi-Fi hotspot')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Apple CarPlay')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Autopilot for Self-Driving')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Features");
+        }
+    }
+}
