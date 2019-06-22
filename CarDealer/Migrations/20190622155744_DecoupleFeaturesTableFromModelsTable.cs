4c8dab696cf89a8da57ef7273c78e13e4087be89
polaris87 <polarisdev87@outlook.com>
2019-06-22 17:58:03 +0200

    Created Migration 'DecoupleFeaturesTableFromModelsTable'

---
+++ CarDealer/Migrations/20190622155744_DecoupleFeaturesTableFromModelsTable.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarDealer.Migrations
+{
+    public partial class DecoupleFeaturesTableFromModelsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Features_Models_ModelId",
+                table: "Features");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Features_ModelId",
+                table: "Features");
+
+            migrationBuilder.DropColumn(
+                name: "ModelId",
+                table: "Features");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ModelId",
+                table: "Features",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Features_ModelId",
+                table: "Features",
+                column: "ModelId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Features_Models_ModelId",
+                table: "Features",
+                column: "ModelId",
+                principalTable: "Models",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
