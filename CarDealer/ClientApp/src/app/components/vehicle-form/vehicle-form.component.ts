7ba51a9d67d9241b23c12ce071babea156edbdaa
polaris87 <polarisdev87@outlook.com>
2019-06-22 17:42:42 +0200

    Display empty array, if no make is selected

--- CarDealer/ClientApp/src/app/components/vehicle-form/vehicle-form.component.ts
+++ CarDealer/ClientApp/src/app/components/vehicle-form/vehicle-form.component.ts
@@ -23,7 +23,7 @@ export class VehicleFormComponent implements OnInit {
   onMakeChange() {
     
     var selectedMake = this.makes.find(m => m.id == this.vehicle.make);
-    this.models = selectedMake.models;
+    this.models = selectedMake ? selectedMake.models : [];
     
   }
 
