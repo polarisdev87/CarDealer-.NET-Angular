99dfffb851b6dd0c1264c8ec56af46860b4959fb
polaris87 <polarisdev87@outlook.com>
2019-06-23 16:11:53 +0200

    Add the first properties to Vehicle DomainModel

--- CarDealer/Core/Domain/Vehicle.cs
+++ CarDealer/Core/Domain/Vehicle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,20 @@ namespace CarDealer.Core.Domain
 {
     public class Vehicle : BaseEntity
     {
+        public int ModelId { get; set; }
+        public Model Model { get; set; }
+
+        public bool IsRegistered { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ContactName { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string ContactPhone { get; set; }
+        [StringLength(255)]
+        public string ContactEmail { get; set; }
+
+        public DateTime LastUpdate { get; set; }
     }
 }
