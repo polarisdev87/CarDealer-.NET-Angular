8581e92e82eb82bd2f7dc36e21ec7edb2de7f5b3
polaris87 <polarisdev87@outlook.com>
2019-06-25 19:01:55 +0200

    Created Vehicle Mapping for Contact Properties

--- CarDealer/Infrastructure/Mapping/MappingProfile.cs
+++ CarDealer/Infrastructure/Mapping/MappingProfile.cs
@@ -13,9 +13,18 @@ using System.Threading.Tasks;
 
         public MappingProfile()
         {
+            // Domain to Dto
             CreateMap<Make, MakeDto>();
             CreateMap<Model, ModelDto>();
             CreateMap<Feature, FeatureDto>();
+
+
+            // Dto to Domain
+            CreateMap<VehicleDto, Vehicle>()
+                .ForMember(v => v.ContactName, opt => opt.MapFrom(vehdto => vehdto.Contact.Name))
+                .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vehdto => vehdto.Contact.Email))
+                .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vehdto => vehdto.Contact.Phone));
+
         }
 
     }
