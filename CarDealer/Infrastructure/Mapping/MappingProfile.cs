1231d187c11fc1f6622aff5c7eadb7a7ef5e3776
polaris87 <polarisdev87@outlook.com>
2019-06-25 19:25:43 +0200

    Using Linq again to Map Vehicle to VehicleDto

--- CarDealer/Infrastructure/Mapping/MappingProfile.cs
+++ CarDealer/Infrastructure/Mapping/MappingProfile.cs
@@ -17,13 +17,19 @@ using System.Threading.Tasks;
             CreateMap<Make, MakeDto>();
             CreateMap<Model, ModelDto>();
             CreateMap<Feature, FeatureDto>();
+            CreateMap<Vehicle, VehicleDto>()
+                .ForMember(vdto => vdto.Contact,
+                    opt => opt.MapFrom(v => new ContactDto
+                        { Name = v.ContactName, Phone = v.ContactPhone, Email = v.ContactEmail }))
+
+                .ForMember(vdto => vdto.Features, opt => opt.MapFrom(v => v.Features.Select(vf => vf.FeatureId)));
 
 
             // Dto to Domain
             CreateMap<VehicleDto, Vehicle>()
-                .ForMember(v => v.ContactName, opt => opt.MapFrom(vehdto => vehdto.Contact.Name))
-                .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vehdto => vehdto.Contact.Email))
-                .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vehdto => vehdto.Contact.Phone))
+                .ForMember(v => v.ContactName, opt => opt.MapFrom(vdto => vdto.Contact.Name))
+                .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vdto => vdto.Contact.Email))
+                .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vdto => vdto.Contact.Phone))
 
                 .ForMember(v => v.Features,
                     opt => opt.MapFrom(vehdto => vehdto.Features.Select(id => new VehicleFeature { FeatureId = id })));
