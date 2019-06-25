256b062d7fd86eb7facbd3f832fce07f09681d6c
polaris87 <polarisdev87@outlook.com>
2019-06-25 18:56:51 +0200

    Added Properties to ContactDto

--- CarDealer/Core/Dto/ContactDto.cs
+++ CarDealer/Core/Dto/ContactDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,15 @@ namespace CarDealer.Core.Dto
 {
     public class ContactDto
     {
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Phone { get; set; }
+
+        [StringLength(255)]
+        public string Email { get; set; }
     }
 }
