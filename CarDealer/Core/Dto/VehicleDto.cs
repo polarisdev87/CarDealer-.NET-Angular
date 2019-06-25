0ca211e034ded98f63f7d35e540866eb32bd8886
polaris87 <polarisdev87@outlook.com>
2019-06-25 18:57:02 +0200

    Added Properties to VehicleDto

---
+++ CarDealer/Core/Dto/VehicleDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using CarDealer.Core.Domain;
+
+namespace CarDealer.Core.Dto
+{
+    public class VehicleDto
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+
+        public bool IsRegistered { get; set; }
+
+        
+        public ContactDto Contact { get; set; }
+
+        public ICollection<int> Features { get; set; }
+
+        public VehicleDto()
+        {
+            Features = new Collection<int>();
+        }
+    }
+}
