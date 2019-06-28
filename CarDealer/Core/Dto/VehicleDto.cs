918a20be557e72a40c4ea5a50561d69f2acb23cc
polaris87 <polarisdev87@outlook.com>
2019-06-28 22:38:22 +0200

    Set up VehicleDto

--- CarDealer/Core/Dto/VehicleDto.cs
+++ CarDealer/Core/Dto/VehicleDto.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using CarDealer.Core.Domain;
 
 namespace CarDealer.Core.Dto
 {
     public class VehicleDto
     {
+
+        public int Id { get; set; }
+
+        public ModelDto Model { get; set; }
+
+        public bool IsRegistered { get; set; }
+
+
+        public ContactDto Contact { get; set; }
+
+        public DateTime LastUpdate { get; set; }
+
+        public ICollection<FeatureDto> Features { get; set; }
+
+
+        public VehicleDto()
+        {
+            Features = new Collection<FeatureDto>();
+        }
+
     }
 }
