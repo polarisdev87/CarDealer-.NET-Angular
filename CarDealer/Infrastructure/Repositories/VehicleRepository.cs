8409fa01ee7abc5ecdddea1b48718d7e502d3938
polaris87 <polarisdev87@outlook.com>
2019-06-29 00:01:02 +0200

    Create VehicleRepository and implement IVehicleRepository

---
+++ CarDealer/Infrastructure/Repositories/VehicleRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarDealer.Core.Domain;
+using CarDealer.Core.Repositories;
+
+namespace CarDealer.Infrastructure.Repositories
+{
+    public class VehicleRepository : Repository<Vehicle>, IVehicleRepository
+    {
+
+        public VehicleRepository(CarDealerDbContext context)
+            : base(context)
+        {
+        }
+
+    }
+}
