26ba77e421979b7d70a545c91ed771070d86d873
polaris87 <polarisdev87@outlook.com>
2019-06-28 23:55:46 +0200

    Create Repository.cs

---
+++ CarDealer/Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarDealer.Infrastructure.Repositories
+{
+    public class Repository
+    {
+    }
+}
