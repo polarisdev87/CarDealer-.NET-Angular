9ca9a78d561dd4a3684a4ca40020cb75238cc491
polaris87 <polarisdev87@outlook.com>
2019-06-28 23:59:37 +0200

    Implement IRepository in Repository Class and implemented the CRUD Methods

--- CarDealer/Infrastructure/Repositories/Repository.cs
+++ CarDealer/Infrastructure/Repositories/Repository.cs
@@ -2,10 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarDealer.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarDealer.Infrastructure.Repositories
 {
-    public class Repository
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
     {
+        private readonly CarDealerDbContext _context;
+
+        public Repository(CarDealerDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return _context.Set<TEntity>().AsNoTracking();
+        }
+
+        public async Task<TEntity> GetById(int id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task Create(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(int id, TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var entity = await GetById(id);
+            _context.Set<TEntity>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
     }
 }
