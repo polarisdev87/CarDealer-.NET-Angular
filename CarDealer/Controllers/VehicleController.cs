0e2a33b68890ac253d9e2ed3ab655b6a71b3d72a
polaris87 <polarisdev87@outlook.com>
2019-06-29 00:20:48 +0200

    Replaced single call with repository call

--- CarDealer/Controllers/VehicleController.cs
+++ CarDealer/Controllers/VehicleController.cs
@@ -31,13 +31,7 @@ namespace CarDealer.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetVehicle(int id)
         {
-            var vehicle = await context.Vehicles
-                .Include(v => v.Features)
-                    .ThenInclude(vf => vf.Feature)
-                .Include(v => v.Model)
-                    .ThenInclude(m => m.Make)
-
-                .SingleOrDefaultAsync(v => v.Id == id);
+            var vehicle = await _repository.GetById(id);
 
             if (vehicle == null)
             {
