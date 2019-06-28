8da69130fd6d7cc609379198fb1a3e0faea1995c
polaris87 <polarisdev87@outlook.com>
2019-06-28 23:28:18 +0200

    Inherit Values for MakeDto from KeyValuePairDto

--- CarDealer/Core/Dto/MakeDto.cs
+++ CarDealer/Core/Dto/MakeDto.cs
@@ -8,10 +8,9 @@ using CarDealer.Core.Domain;
 
 namespace CarDealer.Core.Dto
 {
-    public class MakeDto
+    public class MakeDto : KeyValuePairDto
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+       
         public ICollection<ModelDto> Models { get; set; }
 
 
