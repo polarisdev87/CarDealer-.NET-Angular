c96e80bd9566dfc11aa215bd0fad24bc1490a61d
polaris87 <polarisdev87@outlook.com>
2019-06-29 17:28:19 +0200

    Derive from Repository and implement IModelRepository

--- CarDealer/Infrastructure/Repositories/ModelRepository.cs
+++ CarDealer/Infrastructure/Repositories/ModelRepository.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarDealer.Core.Domain;
+using CarDealer.Core.Repositories;
 
 namespace CarDealer.Infrastructure.Repositories
 {
-    public class ModelRepository
+    public class ModelRepository : Repository<Model>, IModelRepository
     {
     }
 }
