4614093930179b29c00512b8cd8c42010b50c10c
polaris87 <polarisdev87@outlook.com>
2019-06-29 18:20:13 +0200

    Inject context and Implement IUnitOfWork in UnitOfWork Class

--- CarDealer/Infrastructure/UnitOfWork.cs
+++ CarDealer/Infrastructure/UnitOfWork.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarDealer.Core;
 
 namespace CarDealer.Infrastructure
 {
-    public class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+
+        public readonly CarDealerDbContext _context;
+
+        public UnitOfWork(CarDealerDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task Complete()
+        {
+            await _context.SaveChangesAsync();
+        }
+
     }
 }
