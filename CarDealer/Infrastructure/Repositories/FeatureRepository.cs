b2463193b8088fb67e7b949017203429b4fd4c05
polaris87 <polarisdev87@outlook.com>
2019-06-30 18:35:52 +0200

    Derive from Repository and implement IFeatureRepository

--- CarDealer/Infrastructure/Repositories/FeatureRepository.cs
+++ CarDealer/Infrastructure/Repositories/FeatureRepository.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarDealer.Core.Domain;
+using CarDealer.Core.Repositories;
 
 namespace CarDealer.Infrastructure.Repositories
 {
-    public class FeatureRepository
+    public class FeatureRepository : Repository<Feature>, IFeatureRepository
     {
+
+
     }
 }
