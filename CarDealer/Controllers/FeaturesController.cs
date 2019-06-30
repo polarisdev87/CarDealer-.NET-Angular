17427bac8466587266f2b9dafd37a75ac40f1dee
polaris87 <polarisdev87@outlook.com>
2019-06-30 19:05:46 +0200

    Removed not used Usings

--- CarDealer/Controllers/FeaturesController.cs
+++ CarDealer/Controllers/FeaturesController.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using CarDealer.Core;
 using CarDealer.Core.Domain;
 using CarDealer.Core.Dto;
 using CarDealer.Core.Repositories;
-using CarDealer.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace CarDealer.Controllers
 {
