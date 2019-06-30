a05bef838e2c35d85bf68d3a8650562be6615518
polaris87 <polarisdev87@outlook.com>
2019-06-30 18:58:08 +0200

    Inject Context in MakeRepository

--- CarDealer/Infrastructure/Repositories/MakeRepository.cs
+++ CarDealer/Infrastructure/Repositories/MakeRepository.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarDealer.Core.Domain;
+using CarDealer.Core.Repositories;
 
 namespace CarDealer.Infrastructure.Repositories
 {
-    public class MakeRepository
+    public class MakeRepository : Repository<Make>, IMakeRepository
     {
+
+        public MakeRepository(CarDealerDbContext context)
+            : base(context)
+        {
+
+        }
+
     }
 }
