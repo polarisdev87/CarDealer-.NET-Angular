71df0e899fd4968c773690e9d905e6172e42f14c
polaris87 <polarisdev87@outlook.com>
2019-07-07 13:27:08 +0200

    Inject ToastyService in constructor of vehicle-form.component

--- CarDealer/ClientApp/src/app/app.module.ts
+++ CarDealer/ClientApp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastyModule } from 'ng2-toasty';
 
 import { VehicleService } from './services/vehicle.service';
 
@@ -26,6 +27,7 @@ import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.com
     HttpModule,
     HttpClientModule,
     FormsModule,
+    ToastyModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'vehicles/new', component: VehicleFormComponent }
