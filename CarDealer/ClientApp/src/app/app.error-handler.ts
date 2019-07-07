5207e45a11838e4b2ddc4bfa7dd0d90ff7f12a7a
polaris87 <polarisdev87@outlook.com>
2019-07-07 13:41:22 +0200

    Add error handling code to app.error-handler and print error on console

--- CarDealer/ClientApp/src/app/app.error-handler.ts
+++ CarDealer/ClientApp/src/app/app.error-handler.ts
@@ -4,6 +4,16 @@ export class AppErrorHandler implements ErrorHandler {
 
   handleError(error: any): void {
 
+    console.log("ERROR");
+    /*
+    this.toastyService.error({
+      title: 'Error',
+      msg: 'An unexpected Error happened.',
+      theme: 'bootstrap',
+      showClose: true,
+      timeout: 5000
+    });
+    */
   }
 
 }
